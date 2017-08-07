78cdb8b1d4a65aeafe4d956cdcb4e2a0a5b4e4dc
haki-projects <30647767+haki-projects@users.noreply.github.com>
2017-08-07 17:12:47 +0200

    added form validation
    Added form validation to the post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,79 @@
 import React, { Component } from 'react';
-
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+
+  renderField(field) {
+    const className = `form-group ${field.meta.touched && field.meta.error ? 'has-danger' : ''}`
+      return (
+        <div className='form-group has-danger'>
+        <label>{field.label}</label>
+        <input className='form-control'
+        type={field.type}
+          {...field.input}
+        />
+        <div className='text-help'>
+        {field.meta.touched ? field.meta.error: ''}
+        </div>
+        </div>
+      );
+  }
+    onSubmit(values) {
+        console.log(values);
+    }
+
   render() {
+    const { handleSubmit } = this.props;
     return (
-      <div>Posts New!</div>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label='Title'
+          name='title'
+          type='text'
+          component={this.renderField}
+          />
+          <Field
+          label='Categories'
+          name='categories'
+          type='text'
+          component={this.renderField}
+          />
+            <Field
+          label='Content'
+          name='content'
+          type='text'
+          component={this.renderField}
+          />
+          <button type='submit' className='btn btn-primary'>Submit</button>
+      </form>
     );
   }
 }
+/**
+ * used to validate the form input by using if statements to check values.fieldName's for whatever we
+ * want to validate. For now, we are checking the fields to make sure they are not empty
+ * Any errors will be added to the errors object, and the error messages will be displayed under the
+ * corresponding field
+ * @param {*} values : all the values the user has entered into the form
+ */
+function validate(values){
+    const errors={};
 
-export default PostsNew;
+    //Validate the inputs from 'values'
+      if(!values.title){
+          errors.title = "Enter a title!";
+      }
+         if(!values.categories){
+          errors.categories = "Enter a category!";
+      }
+         if(!values.content){
+          errors.content = "Enter some content!";
+      }
+
+    return errors;
+}
+//The validate function is run when the form is submitted and checks the form for the if statements above
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm' //make sure the string assigned to the form property is unique.
+})(PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from '../reducers/reducer_posts';
+
+
 const rootReducer = combineReducers({
   posts: PostsReducer,
-  robbieConstant: "123456"
+  form: formReducer //NOTE: make sure that the keyword is 'form' as this keyword is used within redux-form
 
 });
 
